a50b98aaf797924d067d250c2085926a8e09cb8f
p.shcharbakov <pawel4post@gmail.com>
2021-01-17 16:11:47 +0300

    add global exception handler

--- src/main/java/com/ataccama/controller/DatabaseDetailController.java
+++ src/main/java/com/ataccama/controller/DatabaseDetailController.java
@@ -5,14 +5,12 @@ import com.ataccama.service.DatabaseDetailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -58,12 +56,6 @@ public class DatabaseDetailController {
         return dbDetailService.findById(uuid)
                               .map(ResponseEntity::ok)
                               .orElse(ResponseEntity.noContent().build());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public String handleException(Exception exception) {
-        return exception.getMessage();
     }
 
 }
---
+++ src/main/java/com/ataccama/controller/ExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.ataccama.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
